10d253c4b5b4a0b114986674c7a1da5c2d6d193a
trevor727 <richard.omondi@starthmore.edu>
2022-07-23 20:32:10 +0300

    git commit -m "created attributes and constructors for Doctor.java"

--- src/main/java/com/example/hospital/Model/Doctor.java
+++ src/main/java/com/example/hospital/Model/Doctor.java
@@ -1,4 +1,26 @@
 package com.example.hospital.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@SuppressWarnings("ALL")
+
 public class Doctor {
+    private Long doctor_id;
+    private String name;
+    private Long age;
+    private String consult;
+    private String gender;
+    private String speciality;
+
+    public Doctor(Long doctor_id, String name, Long age, String consult, String gender, String speciality) {
+        this.doctor_id = doctor_id;
+        this.name = name;
+        this.age = age;
+        this.consult = consult;
+        this.gender = gender;
+        this.speciality = speciality;
+    }
 }
