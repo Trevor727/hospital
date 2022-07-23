d07ad94a9e7228230baa937ad66c1c1acd58ae6a
trevor727 <richard.omondi@starthmore.edu>
2022-07-23 12:05:06 +0300

    git commit -m "added attributes to patient.java"

--- src/main/java/com/example/hospital/Model/patient.java
+++ src/main/java/com/example/hospital/Model/patient.java
@@ -1,4 +1,17 @@
 package com.example.hospital.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "patient")
+@SuppressWarnings("ALL")
+
 public class patient {
+   private Long patient_id;
+   private String name;
+   private Long age;
+   private String gender;
+   private Long tel;
+   private String sicknessdetails;
 }
