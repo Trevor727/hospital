1c9eff75577035f750995739d648c4b14196d880
trevor727 <richard.omondi@starthmore.edu>
2022-07-23 12:08:43 +0300

    git commit -m "generated constructors in  patient.java"

--- src/main/java/com/example/hospital/Model/patient.java
+++ src/main/java/com/example/hospital/Model/patient.java
@@ -14,4 +14,13 @@ public class patient {
    private String gender;
    private Long tel;
    private String sicknessdetails;
+
+   public patient(Long patient_id, String name, Long age, String gender, Long tel, String sicknessdetails) {
+      this.patient_id = patient_id;
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+      this.tel = tel;
+      this.sicknessdetails = sicknessdetails;
+   }
 }
